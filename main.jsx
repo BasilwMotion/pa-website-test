74a5be10d762321a20a325b7f390b80bf121000e
BasilwMotion <basilwmotion@gmail.com>
2026-06-24 18:38:22 +0200

    refactor: update main.jsx to import from apex-full.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './apex-app.jsx'
+import App from './apex-full.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
